863ac889c3ca839883629c1654d8d909a9a85d7e
Shreyas Bapat <bapat.shreyas@gmail.com>
2020-07-06 03:08:53 +0530

    fix

--- app.py
+++ app.py
@@ -10,28 +10,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-
-
-import numpy as np
-import astropy.units as u
-
-from einsteinpy.plotting import GeodesicPlotter
-from einsteinpy.coordinates import SphericalDifferential
-from einsteinpy.bodies import Body
-from einsteinpy.geodesic import Geodesic
-
 import pickle
-
-def perihelion():
-    Attractor = Body(name="BH", mass=6e24*u.kg, parent=None)
-    sph_obj = SphericalDifferential(130*u.m, np.pi/2*u.rad, -np.pi/8*u.rad,
-                                    0*u.m/u.s, 0*u.rad/u.s, 1900*u.rad/u.s)
-    Object = Body(differential=sph_obj, parent=Attractor)
-    geodesic = Geodesic(body=Object, time=0 * u.s, end_lambda=0.002, step_size=5e-8)
-
-    obj = GeodesicPlotter()
-    obj.plot(geodesic)
-    return obj.fig
 
 with open('trajectory.pkl', 'rb') as input:
     obj = pickle.load(input)
